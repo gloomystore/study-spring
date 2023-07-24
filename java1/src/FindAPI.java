8fc0587aa3b946debea68c771ad5d110c2fc9fe4
gloomystore <pikchu3333@gmail.com>
2023-07-25 00:01:35 +0900

    2진수, 8진수, 16진수

---
+++ java1/src/FindAPI.java
@@ -0,0 +1,8 @@
+public class FindAPI {
+    public static void main(String[] args) {
+        int a = 10;
+        String s = "11234";
+        // java.lang.String s = "11234"; 와 같은 의미
+        System.out.println(s.length());
+    }
+}
