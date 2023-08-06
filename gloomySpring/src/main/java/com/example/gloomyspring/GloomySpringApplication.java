46544f93f333f6dafcab29d75dbd2cca9d216db3
gloomystore <pikchu3333@gmail.com>
2023-08-06 15:56:39 +0900

    reflect

---
+++ gloomySpring/src/main/java/com/example/gloomyspring/GloomySpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.gloomyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloomySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GloomySpringApplication.class, args);
+    }
+
+}
